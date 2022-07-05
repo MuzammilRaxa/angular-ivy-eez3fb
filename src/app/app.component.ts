ca73c24db83b2aeb127275e2f6a94a61afa0586d
MuzammilRaza126 <razamuzammil426@gmail.com>
2022-07-05 14:21:51 +0500

    3rd change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  count(e: Event) {
+    let countNumber = 0;
+    console.log(countNumber);
+  }
 }
